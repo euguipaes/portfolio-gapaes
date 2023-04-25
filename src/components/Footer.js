d55b817fba6cbf007582ff2e5b98cdce42dd4262
Guilherme Paes <127236494+euguipaes@users.noreply.github.com>
2023-04-25 16:10:38 -0300

    Botão do WhatsApp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.png";
 import navIcon1 from "../assets/img/nav-icon1.png";
 import navIcon2 from "../assets/img/nav-icon2.png";
+import navIcon3 from "../assets/img/nav-icon3.png";
 
 export const Footer = () => {
   return (
@@ -13,8 +14,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https://www.linkedin.com/in/guilherme-paes-7b5591133/"><img src={navIcon1} alt="Linkedin" /></a>
-              <a href="https://github.com/euguipaes"><img src={navIcon2} alt="Github" /></a>
+              <a href="https://www.linkedin.com/in/guilherme-paes-7b5591133/" target="_blank"><img src={navIcon1} alt="Linkedin" /></a>
+              <a href="https://github.com/euguipaes" target="_blank"><img src={navIcon2} alt="Github" /></a>
+              <a href="https://api.whatsapp.com/send?phone=5511975966009" target="_blank"><img src={navIcon3} alt="WhatsApp" /></a>
             </div>
             <p>Copyright 2023. Todos os direitos reservados. | Guilherme Paes.</p>
           </Col>
