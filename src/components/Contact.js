18cc02aac1ced3c244b099b0fec0e310087969b8
Guilherme Paes <127236494+euguipaes@users.noreply.github.com>
2023-07-02 15:24:57 -0300

    Add files via upload

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,50 +1,146 @@
+import React, { useState, useRef } from "react";
+import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact.JPG";
-import TrackVisibility from 'react-on-screen';
-import cv from "../assets/cv.pdf";
-<script src="https://kit.fontawesome.com/0b64daf7ff.js" crossorigin="anonymous"></script>
+import "animate.css";
+import "../App.css";
+import emailjs from "@emailjs/browser";
 
 export const Contact = () => {
-  
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = {
+      name,
+      email,
+      phone,
+      message,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://cesararquitetura.com.br/send",
+        formData
+      );
+
+      if (response.status === 200) {
+        setStatus({
+          success: true,
+          message:
+            "Obrigado pela mensagem. Entraremos em contato o mais rápido possível.",
+        });
+      } else {
+        setStatus({
+          success: false,
+          message: "Algo deu errado, tente novamente mais tarde.",
+        });
+      }
+    } catch (error) {
+      setStatus({
+        success: false,
+        message: "Algo deu errado, tente novamente mais tarde.",
+      });
+    }
+  };
+
+  function enviarEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_sfh14is",
+        "template_f1s2z4q",
+        form.current,
+        "cXFe9R0DgvKYnMu5q"
+      )
+      .then(
+        (result) => {
+          setStatus({
+            success: true,
+            message:
+              "Obrigado pela mensagem. Entraremos em contato o mais rápido possível.",
+          });
+        },
+        (error) => {
+          setStatus({
+            success: false,
+            message: "Algo deu errado, tente novamente mais tarde.",
+          });
+        }
+      );
+    e.target.reset();
+  }
+
   return (
     <section className="contact" id="connect">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact"/>
-              }
-            </TrackVisibility>
+          <Col size={12} md={5}>
+            <img src={contactImg} alt="Contact Us" />
           </Col>
           <Col size={12} md={6}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <h2>Entre em contato comigo!</h2>
-                <h3>Terei prazer em te atender.</h3><br></br>
-                  <Row>
-                    <Col size={12} sm={12} className="px-1">
-                      <h4><i class="fa-brands fa-whatsapp"></i> (11) 97596-6009</h4>
+            <h2>Entre em contato</h2>
+            <form ref={form} onSubmit={enviarEmail}>
+              <Row>
+                <Col size={12} sm={12} className="px-1">
+                  <input
+                    name="name"
+                    type="text"
+                    placeholder="Nome"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </Col>
+                <Col size={12} sm={6} className="px-1">
+                  <input
+                    name="email"
+                    type="email"
+                    placeholder="E-mail"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Col>
+                <Col size={12} sm={6} className="px-1">
+                  <input
+                    name="phone"
+                    type="tel"
+                    placeholder="Telefone"
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </Col>
+                <Col size={12} className="px-1">
+                  <textarea
+                    rows="6"
+                    name="message"
+                    placeholder="Mensagem"
+                    onChange={(e) => setMessage(e.target.value)}
+                  ></textarea>
+                  <button type="submit" id="send">
+                    <span>Enviar</span>
+                  </button>
+                </Col>
+                <Row>
+                  {status && (
+                    <Col>
+                      <p
+                        className={status.success ? "success" : "error"}
+                        id="emailStatusMessage"
+                      >
+                        {status.message}
+                      </p>
                     </Col>
-                    <Col size={12} sm={12} className="px-1">
-                      <h4><i class="fa-regular fa-envelope"></i> contatogapaes@gmail.com</h4>
-                    </Col>
-                    <Col size={12} sm={12} className="px-1">
-                      <br/>
-                      <h4><i class="fa-solid fa-house"></i> São Paulo, São Paulo - Brasil</h4>
-                    </Col>
-                    <Col size={12} className="px-1">
-                    <div className="contact-button">
-                      <button><a href={cv} target="_blank">DOWNLOAD CV</a></button>
-                    </div>
-                    </Col>
-                  </Row>
-              </div>}
-            </TrackVisibility>
+                  )}
+                </Row>
+              </Row>
+            </form>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
